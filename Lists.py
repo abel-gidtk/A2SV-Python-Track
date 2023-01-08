06369f75c015c1e1a8bbc6d88cf80af80d40027f
Abel Gideon <112166909+abel-gidtk@users.noreply.github.com>
2023-01-08 13:21:33 +0300

    Create Lists.py

---
+++ Lists.py
@@ -0,0 +1,23 @@
+import sys
+if __name__ == '__main__':
+    li = list()
+    N = int(input())
+    for i in range(N):
+        command = str(input())
+        if ("insert" in command):
+            new_c = command.split(" ");
+            li.insert(int(new_c[1]), int(new_c[2]))
+        elif("print" in command):
+            print(li)
+        elif("sort" in command):
+            li.sort()
+        elif("pop" in command):
+            li.pop()
+        elif("append" in command):
+            new_c = command.split(" ")
+            li.append(int(new_c[1]))
+        elif("reverse" in command):
+            li.reverse()
+        elif("remove" in command):
+            new_c = command.split(" ")
+            li.remove(int(new_c[1]))
