31cf99d6d96b981dcd392034ef715a06bc4117bf
Abel Gideon <112166909+abel-gidtk@users.noreply.github.com>
2023-01-08 18:58:13 +0300

    Create Integers-In-All-Sizes.py

---
+++ Integers-In-All-Sizes.py
@@ -0,0 +1,7 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+num1 = int(input())
+num2 = int(input())
+num3 = int(input())
+num4 = int(input())
+
+print(pow(num1, num2) + pow(num3, num4))
